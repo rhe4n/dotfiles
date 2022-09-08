22fad0308424775130044f74848a13b50b0a9a6d
rhe4n <rhe4ndhont@gmail.com>
2022-09-08 13:14:53 +0200

    ww

--- dot_config/polybar/scripts/executable_openvpn-status.sh
+++ dot_config/polybar/scripts/executable_openvpn-status.sh
@@ -6,8 +6,8 @@ LABEL="VPN: "
 CONNNAME=$(pgrep -a openvpn$ | head -n 1 | awk '{print $NF }' | cut -d '.' -f 1)
 ICON="嬨"
 COLOR="#a9dd96"
-CONNECT="canvas_connect"
-DISCONNECT="canvas_disconnect"
+CONNECT="nimbus_connect"
+DISCONNECT="nimbus_disconnect"
 
 if [[ -n $CONNNAME ]]; then
 	printf '%s\n' "%{u"$COLOR"}%{+u}%{F"$COLOR"}"$ICON"%{F-}  "$CONNNAME"%{u-}"
